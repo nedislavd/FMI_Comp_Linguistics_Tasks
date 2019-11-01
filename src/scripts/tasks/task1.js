13bc371a8e202312aaf0643a935de42ccefec00a
nedislavd <nedislav.denev@gmail.com>
2019-11-01 20:53:57 +0200

    Task 1 Complete. Added default value, onchange conversion for text input and currency selection.

--- src/scripts/tasks/task1.js
+++ src/scripts/tasks/task1.js
@@ -9,15 +9,17 @@
 'use strict';
 
 import * as currencyMap from './task1_currency_db_map.json';
+import currency from 'currency.js';
 
 //TODO: cycle through all matches, extract symbol, match to currency rate and replace the whole string
 
 class CurrencyFinder {
-    constructor(userInput, currencyBase, dropdownSelector, supportedSymbolsSelector) {
+    constructor(userInput, currencyBase, dropdownSelector, supportedSymbolsSelector, $outputElSelector) {
         this.userInput = userInput;
         this.currencyBase = currencyBase;
         this.dropdownSelector = dropdownSelector;
         this.supportedSymbolsSelector = supportedSymbolsSelector;
+        this.$outputElSelector = $outputElSelector;
     }
 
     /**
@@ -33,11 +35,11 @@ class CurrencyFinder {
             });
     }
 
-    toUnicode(strChar) {
+    /*toUnicode(strChar) {
         if (strChar.length < 4)
             return strChar.codePointAt(0).toString(16);
         return strChar.codePointAt(0).toString(16) + '-' + strChar.codePointAt(2).toString(16);
-    }
+    }*/
 
     /**
      * Select a <select> element and populate its options from array of strings
@@ -69,6 +71,8 @@ class CurrencyFinder {
      * */
     findCurrency(exchangeRate) {
         console.log('Fetched Exchange Rate is:', exchangeRate);
+        let $outputEl = document.getElementById(this.$outputElSelector);
+        this.exchangeRate = exchangeRate;
         const regExCurrencySymbols = '\\$\\£\\€';
         let availableCurrencies = [];
         // let currencySymbols = [];
@@ -91,40 +95,74 @@ class CurrencyFinder {
         // todo: cycle through and find all matches
         console.log('User input is: ', this.userInput);
         let matchedCurrencyArray = this.userInput.match(currencyRegEx);
+        if (!matchedCurrencyArray) {
+            $outputEl.value = this.userInput;
+            return;
+        }
         console.log('Match is ', matchedCurrencyArray);
         // set input matched currencies
-        let numbersToConvert = {
-            'USD': 0,
-            'GBP': 0,
-            'EUR': 0,
-        };
+        let numbersToConvert = [];
         matchedCurrencyArray.forEach((match) => {
             let numberPart = match.match(/\d/g);
             numberPart = numberPart.join('');
             if (match.match(/\$/g)) {
-                numbersToConvert['USD'] += parseFloat(numberPart);
+                numbersToConvert.push({currencyType: 'USD', value: numberPart});
             } else if (match.match(/\£/g)) {
-                numbersToConvert['GBP'] += parseFloat(numberPart);
+                numbersToConvert.push({currencyType: 'GBP', value: numberPart});
             } else if (match.match(/\€/g)) {
-                numbersToConvert['EUR'] += parseFloat(numberPart);
+                numbersToConvert.push({currencyType: 'EUR', value: numberPart});
             }
         });
         console.log('Only number matches are: ', numbersToConvert);
         // calculate in output currency
-        // return this.userInput.match(currencyRegEx);
         let convertedCurrencies = this.convertCurrency(numbersToConvert);
-        // todo: build back the initial string and return it
+        console.log('Converted currency to ', this.currencyBase);
+        console.log(convertedCurrencies);
+        let convertedString = this.userInput;
+        // replace converted currency into original string
+        matchedCurrencyArray.forEach((matchedEl, index) => {
+            if (convertedString.includes(matchedEl)) {
+                convertedString = convertedString.replace(matchedEl, convertedCurrencies[index]);
+            }
+        });
+        console.log('Final output string is: ', convertedString);
+        // return convertedString;
+        $outputEl.value = convertedString;
     }
 
+
     //todo: implement
-    convertCurrency(currencyObj) {
-        return;
+    convertCurrency(matchedCurrencyArray) {
+        const convertToBase = value => currency(value, {symbol: `${currencyMap.default[this.currencyBase].symbol_native}`, precision: 2, formatWithSymbol: true});
+
+        console.log('Converting to ', this.currencyBase);
+        console.log('Using conversion table: ', this.exchangeRate);
+        let convertedValues = [];
+
+        matchedCurrencyArray.forEach((matchedEl, index) => {
+            for (let [keyRate, valueRate] of Object.entries(this.exchangeRate.rates)) {
+
+                if (matchedEl.currencyType === keyRate && matchedEl.value != 0) {
+                    console.log('Matching Currency!', matchedEl.currencyType, keyRate);
+                    console.log('from ', matchedEl.value, ' to ', valueRate);
+                    let convertedNumber = 0;
+                    convertedNumber = convertToBase(currency(currency(matchedEl.value).multiply(valueRate))).format(true);
+
+                    convertedValues[index] = convertedNumber;
+                } else if (this.currencyBase === matchedEl.currencyType) {
+                    convertedValues[index] = convertToBase(matchedEl.value).format(true);
+                }
+            }
+        });
+        // return currencyObj;
+        return convertedValues;
     };
 
     get processedText() {
         this.requestExchangeRate().then((exchangeRateData) => {
             return this.findCurrency(exchangeRateData);
         });
+
     }
 }
 
@@ -133,30 +171,43 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // init default fetched from server;
     let currencyToConvertTo = 'CAD';
     // get DOM elements
+
+    const textOutputSelector = 'outputText';
     const currencySelector = 'selectedCurrency';
     const supportedSymbolsSelector = 'supportedSymbols';
     let $textInput = document.getElementById('inputText');
-    let $textOutput = document.getElementById('outputText');
     let $selectedCurrency = document.getElementById(currencySelector);
     let $convertTrigger = document.getElementById('triggerConversion');
 
-    let currencyFetcher = new CurrencyFinder('Pesho $5 i pesho 20 $','CAD', currencySelector, supportedSymbolsSelector);
+    const testString = `This is a very complex currency converter. 
+                        It took me 6 hours ($ 666 dollars) 
+                        or 677 € oooooooor 
+                        I dont know how many british pounds...
+                        maybe 800£ ...`;
+    $textInput.value = testString;
+
+    let currencyFetcher = new CurrencyFinder(testString,'CAD', currencySelector, supportedSymbolsSelector, textOutputSelector);
     currencyFetcher.processedText;
 
     $selectedCurrency.addEventListener('change', (event) => {
         let currentlySelectedOption = $selectedCurrency[$selectedCurrency.selectedIndex].value;
         currencyToConvertTo = currentlySelectedOption.substr(0, currentlySelectedOption.indexOf(' '));
         console.log('Active Currency: ', currencyToConvertTo);
+        let convertCurrencyOnDropdownChange = new CurrencyFinder($textInput.value, currencyToConvertTo, currencySelector, supportedSymbolsSelector, textOutputSelector);
+        convertCurrencyOnDropdownChange.processedText;
     });
 
     $convertTrigger.addEventListener('click', (event) => {
         event.preventDefault();
-        let stringToConvert = $textInput.value;
-        console.log('Text to convert is: ', stringToConvert, ' into ', currencyToConvertTo);
-        let convertCurrency = new CurrencyFinder(stringToConvert, currencyToConvertTo, currencySelector, supportedSymbolsSelector);
-        let matchedString = convertCurrency.processedText;
-        console.log('match is', matchedString);
-        $textOutput.innerHTML = matchedString;
+        console.log('Text to convert is: ', $textInput.value, ' into ', currencyToConvertTo);
+        let convertCurrencyInstance = new CurrencyFinder($textInput.value, currencyToConvertTo, currencySelector, supportedSymbolsSelector, textOutputSelector);
+        convertCurrencyInstance.processedText;
+    });
+
+    $textInput.addEventListener('keyup', (event) => {
+        console.log('changing input');
+        let convertCurrencyOnChange = new CurrencyFinder($textInput.value, currencyToConvertTo, currencySelector, supportedSymbolsSelector, textOutputSelector);
+        convertCurrencyOnChange.processedText;
     });
 
 });
